4cc7e26f270d4f7209820ff466aa3f3d89879b4c
JustLikeComet <52590393+JustLikeComet@users.noreply.github.com>
2019-07-06 12:49:02 +0800

    Update indentPython.py

--- indentPython.py
+++ indentPython.py
@@ -2,11 +2,12 @@
 import os
 import sys
 import re
+import getopt
 
+
+indentStr = "\t"
 
 def checkIndent(filepath):
-	indentStr = "\t"
-	codeContent = ""
 	indentCount = 0;
 
 	fp = open(filepath, "r")
@@ -29,7 +30,6 @@ def checkIndent(filepath):
 
 
 def indentCode(filepath):
-	indentStr = "\t"
 	codeContent = ""
 	indentCount = 0;
 
@@ -60,16 +60,40 @@ def indentCode(filepath):
 	fo.close()
 
 
-if len(sys.argv) != 2:
-	print ("give me a file path")
-if not os.path.exists(sys.argv[1]):
-	print ("give me a exist file ")
+opts,args = getopt.getopt(sys.argv[1:],'-h-i:',['help','indent='])
 
-indentVal = checkIndent(sys.argv[1])
-if indentVal>0 :
-	print("#{ too many, not close with #}")
-elif indentVal<0 :
-	print("#} too many, not pair with #{")
-else:
-	indentCode(sys.argv[1])
+for opt_name,opt_value in opts:
+	if opt_name in ('-h','--help'):
+		print("indentPython.py [-h|--help] [-i t|sNUM] file [...]")
+		print("-h  --help show help info")
+		print("-i  --indent set indent , \n\t\t-i t use one tab ,\n\t\t -i sNUM is use NUM space NUM in (1,9)")
+		sys.exit()
+	if opt_name in ('-i','--indent'):
+		if opt_value[0] == 's':
+			if opt_value[1]=='0':
+				print('Error with s0 setting')
+				sys.exit()
+			indentStr = ' '*int(opt_value[1])
+		elif opt_value[0] == 't':
+			# do nothing
+			indentStr = "\t"
+		else:
+			print('Error with '+opt_value+' setting')
+			sys.exit()
+
+
+if len(args) == 0:
+	print ("give me file path")
+
+for filepath in args:
+	if not os.path.exists(filepath):
+		print ("give me a exist file ",filepath)
+	else:
+		indentVal = checkIndent(filepath)
+		if indentVal>0 :
+			print("#{ too many, not close with #}")
+		elif indentVal<0 :
+			print("#} too many, not pair with #{")
+		else:
+			indentCode(filepath)
 
